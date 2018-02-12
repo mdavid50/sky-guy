9929a6ae9d4427e22d43ba72c7930db19edc65e9
mdavid5 <mcdav3636@gmail.com>
2018-02-12 16:04:58 -0700

    navbar

--- index.php
+++ index.php
@@ -45,11 +45,28 @@
 <body>
 
 <section class="welcome">
-    <div class="nav">
-        <div class="row">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">SkyGuy</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#">Disabled</a>
+                </li>
+            </ul>
 
         </div>
-    </div>
+    </nav>
+
     <div class="welcome-image">
         <div class="row">
 
@@ -64,4 +81,16 @@
         </div>
     </div>
 </section>
+
+<section class="contact-form">
+    <div class="row">
+
+    </div>
+</section>
 </body>
+<div class="col-4">
+
+</div>
+<div class="col-8">
+
+</div>
