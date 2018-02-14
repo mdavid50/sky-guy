6f841d1a48f311942274a631ed0e57e859202a9d
mdavid5 <mcdav3636@gmail.com>
2018-02-14 15:55:39 -0700

    changed nav background

--- index.php
+++ index.php
@@ -45,8 +45,8 @@
 <body>
 
 <section class="welcome">
-    <nav class="navbar sticky-top navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand text-white" href="#"><h2><img class="bg-primary h-5 text-white" src="images/drone-cam-little.png">SkyGuy</h2></a>
+    <nav class="navbar sticky-top navbar-expand-lg navbar-light bg-dark">
+        <a class="navbar-brand bg-primary text-white" href="#"><h2><img class="bg-primary h-5 text-white" src="images/drone-cam-little.png">SkyGuy</h2></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
